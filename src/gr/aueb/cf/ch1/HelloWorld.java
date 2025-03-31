8d3162c3ad92a1bca56021573f749c93a005c440
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-01 01:05:40 +0300

    first project

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * εμφανιζει τρη φραση Hello World στην κονσολα
+ * (stantdard output)
+ *
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" Coding Factory!");
+        System.out.println();
+        System.out.println("Hello World!");
+
+    }
+}
+
