e87230c20a9e4a0cc2027b07544a1f69ed21f9fa
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-01 20:25:07 +0300

    πρÏfirst program . prints hello world

--- src/gr/aueb/cf/ch1/HelloWorld.java
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -8,9 +8,6 @@ package gr.aueb.cf.ch1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print(" Coding Factory!");
-        System.out.println();
         System.out.println("Hello World!");
 
     }
