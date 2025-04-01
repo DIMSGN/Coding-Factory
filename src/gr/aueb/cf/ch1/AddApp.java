e22ce9ddf967c05f63642e89c4ab792202a25683
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-01 20:21:38 +0300

    variables and string concatenation

---
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Προσθετει δυο ακεραιεους
+ * και εμφανιζει το αποτελεσμα στην κονσολα
+ */
+public class AddApp {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 12;
+        int result = 0;
+
+        result = num1 + num2;
+        System.out.println("Το αποτέλεσμα απο την πρόσθεση " + num1 + " + " + num2 + " είναι: " + result );
+
+    }
+}
