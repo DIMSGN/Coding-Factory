1add3c0ecb6708429a110fa1203a6ac0c79a0650
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-02 03:27:45 +0300

    declare variables , commands and print

--- src/gr/aueb/cf/ch1/AddApp.java
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -1,17 +1,23 @@
 package gr.aueb.cf.ch1;
 
 /**
- * Προσθετει δυο ακεραιεους
- * και εμφανιζει το αποτελεσμα στην κονσολα
+ * Προσθέτει δύο ακεραιέους
+ * και εμφανίζει το αποτέλεσμα στην κονσόλα.
  */
 public class AddApp {
     public static void main(String[] args) {
+
+        //Δήλωση και Αρχικοποίησηση μεταβλητών
         int num1 = 5;
         int num2 = 12;
         int result = 0;
 
+        //Εντολές
         result = num1 + num2;
-        System.out.println("Το αποτέλεσμα απο την πρόσθεση " + num1 + " + " + num2 + " είναι: " + result );
 
+        //Εκτύπωση του αποτελέσματος
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+        System.out.println("Το αποτέλεσμα απο την πρόσθεση " + num1 + " + " + num2 + " είναι: " + result );
+        System.out.printf("Το αποτέλεσμα των: %d και %d  είναι ισο με : %d \n" , num1, num2, result);
     }
 }
