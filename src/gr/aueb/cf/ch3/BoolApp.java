c26d7e3242dcaced26525f2d98ff272eec65fa3c
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-10 10:01:47 +0300

    booleans

---
+++ src/gr/aueb/cf/ch3/BoolApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch3;
+
+/**
+ * Boolean demo
+ */
+
+public class BoolApp {
+    //declare
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean isRaining = false;
+    //print
+        System.out.println(b);
+        System.out.println(isRaining);
+    }
+}
