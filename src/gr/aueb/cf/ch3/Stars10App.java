c1ed7df8bf91e6d0d623fe45f5094729f72e5ee9
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-11 08:24:09 +0300

    while iterations

---
+++ src/gr/aueb/cf/ch3/Stars10App.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch3;
+
+/**
+ * Εμανιζει 10 οριζοντια αστερακια
+ * δηλαδη ενα αστερακι 10 φορες.
+ */
+
+public class Stars10App {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while(i <= 10) {
+            System.out.print("*");
+            i++;
+        }
+
+
+    }
+}
