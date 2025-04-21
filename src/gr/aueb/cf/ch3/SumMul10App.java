6d0cea33091399624bbb81d1c53b14e54898d351
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-21 21:23:57 +0300

    sum mul app

--- src/gr/aueb/cf/ch3/SumMul10App.java
+++ src/gr/aueb/cf/ch3/SumMul10App.java
@@ -1,32 +1,30 @@
 package gr.aueb.cf.ch3;
 
 /**
- * Υπολογιζει το αθροισμα των 10 πρωτων ακεραιων
- * , απο το 1 εως το 10 .
+ * Υπολογίζει το άθροισμα των 10 πρώτων ακεραίων
+ * απο το 1 εως το 10.
  */
 
 public class SumMul10App {
     public static void main(String[] args) {
 
-        int sum= 0;
+        int sum = 0;
         int i = 0;
 
         int mul = 1;
         int j = 1;
 
-        while(i < 10) {
+        while(i <= 10) {
             sum += i;
             i++;
+            System.out.println(sum);
 
-            //System.out.println(i);
+
         }
         while(j <= 10) {
-            sum *= j;
-            System.out.println(j);
+            mul *= j;
             j++;
-
-
+            System.out.println(mul);
         }
-
     }
 }
