4b3aaf4eef4101251b39ae8bc133af6a5bf24563
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-22 18:33:35 +0300

    abs app

---
+++ src/gr/aueb/cf/ch3/AbsTernary.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.ch3;
+
+import java.util.Scanner;
+
+/**
+ * Βρίσκει το απόλυτο ενός ακεραίου με τη
+ * χρήση τριαδικού τελεστή.
+ */
+public class AbsTernary {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int abs;
+
+        System.out.println("Παρακαλώ εισάγετε δύο ακεραίους");
+        num1 = scanner.nextInt();
+
+        abs = (num1 >= 0) ? num1 : -num1;
+
+        System.out.println("Το απόλυτο του " + num1 + " είναι " + abs);
+    }
+}
