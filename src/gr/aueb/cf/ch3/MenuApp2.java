06c763d89a7af4367299060010e3ed88b05a212f
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-22 19:31:12 +0300

    menu app

--- src/gr/aueb/cf/ch3/MenuApp2.java
+++ src/gr/aueb/cf/ch3/MenuApp2.java
@@ -3,43 +3,24 @@ package gr.aueb.cf.ch3;
 import java.util.Scanner;
 
 /**
- * Υλοποιεί ενα μενού με επιλογές και να ελέγχει την επιλογή
- * με if-then-else
+ * Εκφράζει τη λογική της do-while αλλά με
+ * τη χρήση αέναου while-do και sentinel.
  */
-
 public class MenuApp2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int choice = 0;
+        final int EXIT = 3;
+        int choice;
 
-        while(true) {
-            System.out.println("Επιλέξτε ενα απο τα παρακάτω: ");
-            System.out.println("Εισαγωγή");
-            System.out.println("Ενημέρωση");
-            System.out.println("Διαγραφή");
-            System.out.println("Αναζήτηση");
-            System.out.println("Έξοδος");
-
+        while (true) {
+            System.out.println("Παρακαλώ επιλέξτε ένα από τα παρακάτω:");
+            System.out.println("1. Εισαγωγή Προϊόντος");
+            System.out.println("2. Διαγραφή προϊόντος");
+            System.out.println("3. Έξοδος");
             choice = scanner.nextInt();
-
-            if (choice < 1 || choice > 5) {
-                System.out.println("Λάθος επιλογή");
-                continue;
-            }
-            if (choice  == 5) {
-                System.out.println("Ευχαριστούμε");
-                break;
-            }
-            if (choice == 1) {
-                System.out.println("Η εισαγωγή έγινε επιτυχώς.");
-            } else if (choice == 2) {
-                System.out.println("Η ενημέρωση έγινε επιτυχώς.");
-            } else if(choice == 3) {
-                System.out.println("Η διαγραφή έγινε επιτυχώς.");
-            } else {
-                System.out.println("Η αναζήτηση έγινε επιτυχώς.");
-            }
+            if (choice == EXIT) break;
         }
+        System.out.println("Ευχαριστούμε για τη χρήση της εφαρμογής!!");
     }
 }
---
+++ src/gr/aueb/cf/ch3/MenuIf.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.ch3;
+
+import java.util.Scanner;
+
+/**
+ * Υλοποιεί ενα μενού με επιλογές και να ελέγχει την επιλογή
+ * με if-then-else
+ */
+
+public class MenuIf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while(true) {
+            System.out.println("Επιλέξτε ενα απο τα παρακάτω: ");
+            System.out.println("Εισαγωγή");
+            System.out.println("Ενημέρωση");
+            System.out.println("Διαγραφή");
+            System.out.println("Αναζήτηση");
+            System.out.println("Έξοδος");
+
+            choice = scanner.nextInt();
+
+            if (choice < 1 || choice > 5) {
+                System.out.println("Λάθος επιλογή");
+                continue;
+            }
+            if (choice  == 5) {
+                System.out.println("Ευχαριστούμε");
+                break;
+            }
+            if (choice == 1) {
+                System.out.println("Η εισαγωγή έγινε επιτυχώς.");
+            } else if (choice == 2) {
+                System.out.println("Η ενημέρωση έγινε επιτυχώς.");
+            } else if(choice == 3) {
+                System.out.println("Η διαγραφή έγινε επιτυχώς.");
+            } else {
+                System.out.println("Η αναζήτηση έγινε επιτυχώς.");
+            }
+        }
+    }
+}
