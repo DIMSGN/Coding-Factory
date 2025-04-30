a5ebbb44c4523137904f6106aa002f4f22d7d07d
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-05-01 00:27:15 +0300

    chapter 4: introduced chars

---
+++ src/gr/aueb/cf/ch4/CharInput2.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch4;
+
+import java.util.Scanner;
+
+/**
+ * Διαβάζει char με scanner
+ */
+
+public class CharInput2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char inputChar;
+
+        System.out.print("Παρακαλώ εισάγετε χαρακτήρα: ");
+
+        inputChar = scanner.nextLine().charAt(0);
+        System.out.println("Input char: " + inputChar);
+    }
+}
