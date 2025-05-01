fd1ec7283c4c60a3fb7fa167d7d638d7898e3255
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-05-02 00:53:34 +0300

    chapter 4: declare chars

---
+++ src/gr/aueb/cf/ch4/DeclarationApp.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch4;
+
+/**
+ * Δηλώνει και αρχικοποιεί μεταβλητές άτυπου char.
+ */
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+
+        char star = '*';
+        char alpha = 'A';
+        final char EMPTY_CHAR = ' ';
+    }
+}
