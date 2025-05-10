2a7b93813408bdaf0f19eccb4d8a55d525d7da00
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-05-10 22:09:40 +0300

    swap with java

---
+++ src/gr/aueb/cf/ch5/Swap.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.ch5;
+
+/**
+ * Αμοιβαία ανταλλαγή των τιμών δύο μεταβλητών.
+ * Αν έχουμε για παράδειγμα α=5 και b=10, τότε
+ * να γίνει a=10 και b=5.
+ */
+public class Swap {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.printf("a=%d, b=%d\n", a, b);
+//        swap(a, b);
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println();
+        System.out.println("After swap");
+        System.out.println();
+        System.out.printf("a=%d, b=%d\n", a, b);
+    }
+
+    /**
+     * Mutually exchanges the values of the
+     * two input variables.
+     * @param a     the first input.
+     * @param b     the second input.
+     */
+//    public static void swap(int a, int b) {
+//        int tmp = a;
+//        a = b;
+//        b = tmp;
+//    }
+}
