11e99031aa6cf4e86b3096c6dee104f1a9a118d4
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-05-10 22:07:56 +0300

    add sub method app

---
+++ src/gr/aueb/cf/ch5/AddSubWithMethodApp.java
@@ -0,0 +1,52 @@
+package gr.aueb.cf.ch5;
+
+import java.util.Scanner;
+
+/**
+ * Υπολογίζει το άθροισμα των δυο ακεραίων με τη χρήση μεθόδων.
+ *
+ */
+public class AddSubWithMethodApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        int sum;
+        int sub;
+
+        System.out.print("Εισάγετε δυο ακεραίους: ");
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+
+        sum = add(num1, num2);
+        sub = sub(num1, num2);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Sub: " + sub);
+    }
+
+    /**
+     * Adds two integers.
+     * @param a the first integer to add.
+     * @param b the second integer to add.
+     * @return the sum of a and b.
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    /**
+     * Subtracts two integers.
+     * @param a the first integer to subtract.
+     * @param b the second integer that is subtracted.
+     * @return the subtraction of a - b.
+     */
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+//    public static int add(int a, int b) {
+//        int sum = 0;
+//        sum = a + b;
+//        return sum;
+//    }
+}
